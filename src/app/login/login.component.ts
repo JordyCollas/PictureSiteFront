d82cd0343d1a8b0e537b94a892e0eafbb76fec06
jordy <jordy.collas@gmail.com>
2018-04-13 14:50:40 +0200

    fixed navigation return url

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
         this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(
                 data => {
-                    this.router.navigate([this.returnUrl]);
+                    this.router.navigateByUrl(this.returnUrl);
                 },
                 error => {
                     this.alertService.error(error._body);
