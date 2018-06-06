55890abc6a0869ca00dcbafbff228e8a0bfe9ad9
jordy Collas <jordy.collas@gmail.com>
2018-06-06 20:25:57 +0200

    merge fix

--- src/app/models/PictureToDownload.ts
+++ src/app/models/PictureToDownload.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export class PictureToDownload{
     folderName: string;
     pictureName: string;
-=======
-export class PictureToDownload{
-    folderName: string;
-    pictureName: string;
->>>>>>> fa27f2a49a0910b548a1225e4cffba4d98a98f49
 }
